ed0d13c570f144aa7dc5ccb985c1e3866e666cd5
jgh <86884161+jonashojer@users.noreply.github.com>
2023-10-24 10:44:09 +0200

    Lavet en statisk plade

--- banko/Models/Plates.cs
+++ banko/Models/Plates.cs
@@ -24,7 +24,35 @@ namespace banko.Models {
 			}
 		}
 
-		private static List<int?[]> GenerateNumbers() {
+		public void StaticPlate() {
+			// Tal er taget fra seedet "jonas"
+			int[,] rows = { 
+				{ 1, 0, 21, 31, 40, 0, 0, 0, 80 }, 
+				{ 0, 13, 0, 0, 0, 51, 64, 72, 88 }, 
+				{ 0, 18, 0, 0, 49, 59, 68, 0, 90 },
+			};
+
+			Console.WriteLine(rows[0, 3]);
+
+			Console.WriteLine("/--------------------------------------------\\");
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j > 9; j++) {
+					if (j == 8) {
+						Console.Write($" {rows[i, j]} |\n");
+					} else if (j == 0) {
+						Console.Write($"| {rows[i, j]} ");
+					} else {
+						Console.Write($" {rows[i, j]} |");
+					}
+					if (i != 2) {
+						Console.WriteLine(" ----------------------------------------");
+					}
+				}
+			}
+			Console.WriteLine("\\--------------------------------------------/\n");
+		}
+
+        private static List<int?[]> GenerateNumbers() {
 			List<int?[]> numbersResult = new List<int?[]>();
 			Random random = new Random();
 			int randMin = 1;
--- banko/Program.cs
+++ banko/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
       var hest = new Plates();
-      hest.GeneratePlate(2);
+      hest.StaticPlate();
     }
 }
 
