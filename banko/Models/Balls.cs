35fb8d3156f9b8279188aee7f84368bf177f3cab
jgh <86884161+jonashojer@users.noreply.github.com>
2023-10-24 13:33:37 +0200

    giver op på oop

---
+++ banko/Models/Balls.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banko.Models {
+    public class Balls {
+        public int amount { get; set; }
+
+        public static List<int[]> GenerateBalls(int amount = 5) {
+            List<int[]> numbersResult = new List<int[]>();
+            Random random = new Random();
+            int randMin = 1;
+            int randMax = 91;
+
+            for (int i = 0; i < amount; i++) {
+                List<int> numbersAdded = new List<int>();
+                while (true) {
+                    int result = random.Next(randMin, randMax);
+                    if (!numbersAdded.Contains(result)) {
+                        numbersAdded.Add(result);
+                        break;
+                    }
+                }
+                int[] array = numbersAdded.ToArray();
+                numbersResult.Add(array);
+            }
+            return numbersResult;        
+        }
+        public void DrawBalls(List<int> balls)
+        {
+            // Print the number of each ball to the console
+            foreach (int ball in balls)
+            {
+                Console.Write(ball);
+            }
+        }
+    }
+
+}
--- banko/Program.cs
+++ banko/Program.cs
@@ -2,12 +2,88 @@
 
 namespace banko;
 
-class Program
-{
-    static void Main(string[] args)
+class Program {
+    static void Main(string[] args) {
+      			// Tal er taget fra seedet "jonas"
+			int[,] rows = { 
+				{ 1, 21, 31, 40, 80 }, 
+				{ 13, 51, 64, 72, 88 }, 
+				{ 18, 49, 59, 68, 90 },
+			};
+
+			Console.WriteLine("Seed: jonas");
+			Console.WriteLine("/--------------------------------------------\\");
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 5; j++) {
+					if (j == 4) {
+						if (rows[i, j] == 0) {
+							Console.Write("    |\n");
+						} else {
+							Console.Write($" {rows[i, j]} |\n");
+						}
+					} else if (j == 0) {
+                        if (rows[i, j] == 0) {
+                            Console.Write("|    |");
+                        } else {
+                            Console.Write($"| {rows[i, j]}  |");
+                        }
+					} else {
+                        if (rows[i, j] == 0) {
+                            Console.Write("    |");
+                        } else {
+                            Console.Write($" {rows[i, j]} |");
+                        }
+                        
+					}
+				}
+                if (i != 2)
+                {
+                    Console.WriteLine(" --------------------------------------------");
+                }
+            }
+			Console.WriteLine("\\--------------------------------------------/\n");
+
+            bool[,] fieldMarked = new bool[3, 5];
+            bool[] rowBingo = new bool[3];
+
+
+        while (true)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Mark the number on the corresponding board
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (rows[i, j] == number)
+                    {
+                        fieldMarked[i, j] = true;
+                    }
+                }
+            }
+            // Check for banko on each row and update the row banko status
+            for (int i = 0; i < 3; i++)
+            {
+                if (!rowBingo[i] && CheckBankoRow(fieldMarked, i))
+                {
+                    Console.WriteLine($"You've bingo on row {i + 1}!");
+                    rowBingo[i] = true;
+                }
+            }
+        }
+    }
+    private static bool CheckBankoRow(bool[,] marked, int row)
     {
-      var hest = new Plates();
-      hest.StaticPlate();
+        for (int j = 0; j < 5; j++)
+        {
+            if (!marked[row, j])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
